835ab4d961727ef013aded7df7d3bb30e75a943a
raulrrv <33469570+raulrrv@users.noreply.github.com>
2020-08-21 23:51:48 -0500

    Codigo Prolog
    Hechos y Reglas de Inferencia

---
+++ Trabajo_Final.pl
@@ -0,0 +1,252 @@
+%hechos
+%marca(iPhone_SE_1G).
+%almacenamiento(32).
+%procesador(a9).
+%doble_sim(si).
+%doble_sim(no).
+%face_id(si).
+%face_id(no).
+%touch_id(si).
+%touch_id(no).
+%ip68(si).
+%ip68(no).
+%anio(2014).
+%anio(2015).
+%anio(2016).
+%precio(200).
+%precio(220).
+%precio(240).
+
+%iPhone SE 1er Gen
+almacenamiento_marca(32, iPhone_SE_1G).
+almacenamiento_marca(64, iPhone_SE_1G).
+almacenamiento_marca(128, iPhone_SE_1G).
+procesador_marca(a9, iPhone_SE_1G).
+sim_marca(no, iPhone_SE_1G).
+face_marca(no, iPhone_SE_1G).
+touch_marca(si, iPhone_SE_1G).
+ip68_marca(no, iPhone_SE_1G).
+anio_marca(2016, iPhone_SE_1G).
+precio_marca(200, iPhone_SE_1G).
+precio_marca(220, iPhone_SE_1G).
+precio_marca(240, iPhone_SE_1G).
+
+%iPhone 6
+almacenamiento_marca(16, iPhone_6).
+almacenamiento_marca(32, iPhone_6).
+almacenamiento_marca(64, iPhone_6).
+procesador_marca(a8, iPhone_6).
+sim_marca(no, iPhone_6).
+face_marca(no, iPhone_6).
+touch_marca(si, iPhone_6).
+ip68_marca(no, iPhone_6).
+anio_marca(2014, iPhone_6).
+precio_marca(180, iPhone_6).
+precio_marca(210, iPhone_6).
+precio_marca(240, iPhone_6).
+
+%iPhone 6 Plus
+almacenamiento_marca(16, iPhone_6_Plus).
+almacenamiento_marca(64, iPhone_6_Plus).
+procesador_marca(a8, iPhone_6_Plus).
+sim_marca(no, iPhone_6_Plus).
+face_marca(no, iPhone_6_Plus).
+touch_marca(si, iPhone_6_Plus).
+ip68_marca(no, iPhone_6_Plus).
+anio_marca(2014, iPhone_6_Plus).
+precio_marca(190, iPhone_6_Plus).
+precio_marca(240, iPhone_6_Plus).
+
+%iPhone 6s
+almacenamiento_marca(32, iPhone_6s).
+almacenamiento_marca(128, iPhone_6s).
+procesador_marca(a9, iPhone_6s).
+sim_marca(no, iPhone_6s).
+face_marca(no, iPhone_6s).
+touch_marca(si, iPhone_6s).
+ip68_marca(no, iPhone_6s).
+anio_marca(2015, iPhone_6s).
+precio_marca(250, iPhone_6s).
+precio_marca(280, iPhone_6s).
+
+%iPhone 6s Plus
+almacenamiento_marca(32, iPhone_6s_Plus).
+almacenamiento_marca(128, iPhone_6s_Plus).
+procesador_marca(a9, iPhone_6s_Plus).
+sim_marca(no, iPhone_6s_Plus).
+face_marca(no, iPhone_6s_Plus).
+touch_marca(si, iPhone_6s_Plus).
+ip68_marca(no, iPhone_6s_Plus).
+anio_marca(2015, iPhone_6s_Plus).
+precio_marca(255, iPhone_6s_Plus).
+precio_marca(290, iPhone_6s_Plus).
+
+%iPhone 7
+almacenamiento_marca(32, iPhone_7).
+almacenamiento_marca(128, iPhone_7).
+procesador_marca(a10, iPhone_7).
+sim_marca(no, iPhone_7).
+face_marca(no, iPhone_7).
+touch_marca(si, iPhone_7).
+ip68_marca(no, iPhone_7).
+anio_marca(2016, iPhone_7).
+precio_marca(280, iPhone_7).
+precio_marca(330, iPhone_7).
+
+%iPhone 7 Plus
+almacenamiento_marca(32, iPhone_7_Plus).
+almacenamiento_marca(128, iPhone_7_Plus).
+procesador_marca(a10, iPhone_7_Plus).
+sim_marca(no, iPhone_7_Plus).
+face_marca(no, iPhone_7_Plus).
+touch_marca(si, iPhone_7_Plus).
+ip68_marca(no, iPhone_7_Plus).
+anio_marca(2016, iPhone_7_Plus).
+precio_marca(320, iPhone_7_Plus).
+precio_marca(370, iPhone_7_Plus).
+
+%iPhone 8
+almacenamiento_marca(64, iPhone_8).
+almacenamiento_marca(128, iPhone_8).
+procesador_marca(a11, iPhone_8).
+sim_marca(no, iPhone_8).
+face_marca(no, iPhone_8).
+touch_marca(si, iPhone_8).
+ip68_marca(no, iPhone_8).
+anio_marca(2017, iPhone_8).
+precio_marca(380, iPhone_8).
+precio_marca(430, iPhone_8).
+
+%iPhone 8 Plus
+almacenamiento_marca(64, iPhone_8_Plus).
+almacenamiento_marca(128, iPhone_8_Plus).
+procesador_marca(a11, iPhone_8_Plus).
+sim_marca(no, iPhone_8_Plus).
+face_marca(no, iPhone_8_Plus).
+touch_marca(si, iPhone_8_Plus).
+ip68_marca(no, iPhone_8_Plus).
+anio_marca(2017, iPhone_8_Plus).
+precio_marca(400, iPhone_8_Plus).
+precio_marca(450, iPhone_8_Plus).
+
+%iPhone X
+almacenamiento_marca(64, iPhone_X).
+almacenamiento_marca(256, iPhone_X).
+procesador_marca(a11, iPhone_X).
+sim_marca(no, iPhone_X).
+face_marca(si, iPhone_X).
+touch_marca(no, iPhone_X).
+ip68_marca(no, iPhone_X).
+anio_marca(2018, iPhone_X).
+precio_marca(450, iPhone_X).
+precio_marca(660, iPhone_X).
+
+%iPhone XR
+almacenamiento_marca(64, iPhone_XR).
+almacenamiento_marca(128, iPhone_XR).
+procesador_marca(a12, iPhone_XR).
+sim_marca(si, iPhone_XR).
+face_marca(si, iPhone_XR).
+touch_marca(no, iPhone_XR).
+ip68_marca(no, iPhone_XR).
+anio_marca(2018, iPhone_XR).
+precio_marca(599, iPhone_XR).
+precio_marca(649, iPhone_XR).
+
+%iPhone XS Max
+almacenamiento_marca(64, iPhone_XS_Max).
+almacenamiento_marca(256, iPhone_XS_Max).
+almacenamiento_marca(512, iPhone_XS_Max).
+procesador_marca(a12, iPhone_XS_Max).
+sim_marca(si, iPhone_XS_Max).
+face_marca(si, iPhone_XS_Max).
+touch_marca(no, iPhone_XS_Max).
+ip68_marca(si, iPhone_XS_Max).
+anio_marca(2018, iPhone_XS_Max).
+precio_marca(630, iPhone_XS_Max).
+precio_marca(839, iPhone_XS_Max).
+precio_marca(998, iPhone_XS_Max).
+
+%iPhone XS
+almacenamiento_marca(64, iPhone_XS).
+almacenamiento_marca(256, iPhone_XS).
+almacenamiento_marca(512, iPhone_XS).
+procesador_marca(a12, iPhone_XS).
+sim_marca(si, iPhone_XS).
+face_marca(si, iPhone_XS).
+touch_marca(no, iPhone_XS).
+ip68_marca(si, iPhone_XS).
+anio_marca(2018, iPhone_XS).
+precio_marca(549, iPhone_XS).
+precio_marca(780, iPhone_XS).
+precio_marca(912, iPhone_XS).
+
+%iPhone SE 2da Gen
+almacenamiento_marca(64, iPhone_SE_2G).
+almacenamiento_marca(128, iPhone_SE_2G).
+almacenamiento_marca(256, iPhone_SE_2G).
+procesador_marca(a13, iPhone_SE_2G).
+sim_marca(si, iPhone_SE_2G).
+face_marca(no, iPhone_SE_2G).
+touch_marca(si, iPhone_SE_2G).
+ip68_marca(no, iPhone_SE_2G).
+anio_marca(2020, iPhone_SE_2G).
+precio_marca(399, iPhone_SE_2G).
+precio_marca(449, iPhone_SE_2G).
+precio_marca(549, iPhone_SE_2G).
+
+%iPhone 11
+almacenamiento_marca(64, iPhone_11).
+almacenamiento_marca(128, iPhone_11).
+almacenamiento_marca(256, iPhone_11).
+procesador_marca(a13, iPhone_11).
+sim_marca(si, iPhone_11).
+face_marca(si, iPhone_11).
+touch_marca(no, iPhone_11).
+ip68_marca(si, iPhone_11).
+anio_marca(2019, iPhone_11).
+precio_marca(699, iPhone_11).
+precio_marca(749, iPhone_11).
+precio_marca(849, iPhone_11).
+
+%iPhone 11 Pro Max
+almacenamiento_marca(64, iPhone_11_Pro_Max).
+almacenamiento_marca(256, iPhone_11_Pro_Max).
+almacenamiento_marca(512, iPhone_11_Pro_Max).
+procesador_marca(a13, iPhone_11_Pro_Max).
+sim_marca(si, iPhone_11_Pro_Max).
+face_marca(si, iPhone_11_Pro_Max).
+touch_marca(no, iPhone_11_Pro_Max).
+ip68_marca(si, iPhone_11_Pro_Max).
+anio_marca(2019, iPhone_11_Pro_Max).
+precio_marca(1099, iPhone_11_Pro_Max).
+precio_marca(1249, iPhone_11_Pro_Max).
+precio_marca(1449, iPhone_11_Pro_Max).
+
+%iPhone 11 Pro
+almacenamiento_marca(64, iPhone_11_Pro).
+almacenamiento_marca(256, iPhone_11_Pro).
+almacenamiento_marca(512, iPhone_11_Pro).
+procesador_marca(a13, iPhone_11_Pro).
+sim_marca(si, iPhone_11_Pro).
+face_marca(si, iPhone_11_Pro).
+touch_marca(no, iPhone_11_Pro).
+ip68_marca(si, iPhone_11_Pro).
+anio_marca(2019, iPhone_11_Pro).
+precio_marca(999, iPhone_11_Pro).
+precio_marca(1149, iPhone_11_Pro).
+precio_marca(1349, iPhone_11_Pro).
+
+
+%reglas de inferencia
+%
+% filtrar_almacenamiento(ALM, TELF):-catch(almacenamiento_marca(X, TELF),ALM =< X, !).
+filtrar_almacenamiento(ALM, TELF):-almacenamiento_marca(X, TELF), ALM =< X.
+%
+%filtrar_almacenamiento(ALM, TELF):-almacenamiento_marca(ALM, TELF).
+
+% definir_precio(VALOR, TELF):-catch(precio_marca(V, TELF), V =< VALOR, !).
+definir_precio(VALOR, TELF):-precio_marca(V, TELF), V =< VALOR.
+
+iphone_recomendado(ALM, VAL, PROC, SIM, FACE, TOUC, IP, ANIO, TELF):-filtrar_almacenamiento(ALM, TELF), procesador_marca(PROC, TELF), sim_marca(SIM, TELF), face_marca(FACE, TELF), touch_marca(TOUC, TELF), ip68_marca(IP, TELF), anio_marca(ANIO, TELF), definir_precio(VAL, TELF).
+
